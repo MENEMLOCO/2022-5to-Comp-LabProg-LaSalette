f145db806cd8e81a05e5ae1fedfc793e7e2c0e13
MENEMLOCO <teogs005@gmail.com>
2022-03-18 14:15:57 -0300

    Cambia .rar a carpetas

--- Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
+++ Ejercicio 7/src/ejercicio/pkg7/Ejercicio7.java
@@ -3,7 +3,7 @@ package ejercicio.pkg7;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Ejercicio7 {
+public class ejercicio7 {
 
     public static void main(String[] args) {
         int a=0;
